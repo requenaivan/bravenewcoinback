40bc944a868d74ee073499934ba96be02e174795
Ivan Requena <ivanjavier1121@gmail.com>
2020-11-03 13:54:49 -0500

    Finalizacion de test

--- braveNewCoin/tests/userRoute.test.js
+++ braveNewCoin/tests/userRoute.test.js
@@ -2,29 +2,34 @@ const request = require('supertest');
 const app = require('../server');
 require('mysql2/node_modules/iconv-lite').encodingExists('foo');
 const User = require("../models").User;
+const Coin = require("../models").Coin;
 
 describe('Post EndPoints', ()=> {
 
     beforeAll(async () => {
-        await User.destroy({
-            where: {},
-            truncate: true
-          });
-      });
+        User.destroy({ where: {}});
+    });
 
-    it('Debe crear un nuevo usuario ', async () => {
-        const res = await request(app).post('/user').send({
+    afterAll(async done => {
+        await User.sequelize.close();
+        app.close();
+        done();
+    });
+
+    it('Debe crear un nuevo usuario ', (done) => {
+        request(app).post('/user').send({
             name: 'ivan',
             lastname: 'requena',
             username: 'ivan',
             password: '12345678',
+          }).then(res => {
+            expect(res.statusCode).toEqual(200);
+            done();
           });
     
-        expect(res.statusCode).toEqual(200);
-        
     });
 
-      it('Validaciones al crear un nuevo usuario ', async () => {
+      it('Validaciones al crear un nuevo usuario ', async done  => {
         const res = await request(app).post('/user').send({
             name: null,
             lastname: 'requena',
@@ -34,7 +39,7 @@ describe('Post EndPoints', ()=> {
     
         expect(res.statusCode).toEqual(400);
         expect(res.text).toEqual(expect.stringContaining("mensaje\":\"KO\""));
-
+        done();
     });
 
     it('Debe realizar el login', async done => {
@@ -59,10 +64,5 @@ describe('Post EndPoints', ()=> {
         
         expect(res.text).toEqual(expect.stringContaining("{\"mensaje\":\"Usuario no encontrado\"}"));
         done();
-    });
-
-    
-    afterAll(() => {User.sequelize.close()
-        User.sequelize.destroy();
     });
 });
